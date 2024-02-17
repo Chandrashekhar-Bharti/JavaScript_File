1724067e8969607a7ebb802285b7136bb7430009
Chandrashekhar Bharti <74781224+Chandrashekhar-Bharti@users.noreply.github.com>
2024-02-17 06:03:35 +0000

    compare operation

---
+++ 01_basics/comparision.js
@@ -0,0 +1,22 @@
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+//  avoid these type of code line 12 to 18 code 
+// === 
+
+console.log("2" === 2);
