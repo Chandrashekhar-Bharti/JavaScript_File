1017e31e20f422bd8c625e7c3dadcbd290f9b3de
Chandrashekhar Bharti <74781224+Chandrashekhar-Bharti@users.noreply.github.com>
2024-02-17 06:58:47 +0000

    some changes

--- 01_basics/num_and_math.js
+++ 01_basics/num_and_math.js
@@ -10,7 +10,9 @@ const balance = new Number(100)
 
 const otherNumber = 123.8966
 
-// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(4)); //toPrecision() is gets the presicion value here is like 123.9 
+const number = 42.56
+console.log(number.toPrecision(3))
 
 const hundreds = 1000000
 // console.log(hundreds.toLocaleString('en-IN'));
@@ -25,11 +27,14 @@ const hundreds = 1000000
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
+console.log(Math.random()); // its getting generally 0 to 1 show output is 0.1,0.2,....
+
+// if you want to print random value b/w 1 to 9
+console.log((Math.random()*10) + 1); //if multiply 10 then one value shift in left side and +1 is not getting only get 1 not below 1
 console.log(Math.floor(Math.random()*10) + 1);
 
 const min = 10
 const max = 20
 
 console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+// (max-min+1) is getting the range and min is getting minimum value
