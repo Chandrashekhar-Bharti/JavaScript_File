7993d94686a8e60094b6f1337a6f18133d0c4790
Chandrashekhar Bharti <74781224+Chandrashekhar-Bharti@users.noreply.github.com>
2024-02-17 06:00:52 +0000

    discussion on variables

---
+++ 01_basics/test.js
@@ -0,0 +1,9 @@
+console.log("hello");
+accountEmail="cas@gmail.com"
+accountpassword="1256"
+accountcity="raipur"
+
+// console.log(); it's print one element at a time
+
+console.table([accountEmail,accountpassword,accountcity]);
+// it print multiple line in using console Table anf print show in table formate
---
+++ 01_basics/variable.js
@@ -0,0 +1,17 @@
+console.log("hello")
+const accountId=2000
+let accountEmail="abs@gmail.com"
+let accountCity="Raipur"
+let accountPassword="5645"
+
+// console.log(); it's print one element at a time
+// accountId = 2 // not allowed because accountId is const
+
+// let variable can be changed because let is used block of scope
+
+accountEmail1 = "hs@sf.com"
+accountPassword2="568922"
+accountCity1="Jaipur"
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+// it print multiple line in using console Table and print show in table formate
+console.table([accountEmail1,accountPassword2,accountCity1]);
