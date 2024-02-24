cdee3756cd13c0b824d00338cc6c508c72335d5a
Chandrashekhar Bharti <74781224+Chandrashekhar-Bharti@users.noreply.github.com>
2024-02-24 18:16:10 +0000

    control flow code files

---
+++ 04_control_flow/switch_case.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {   // if month  is not match the cases then is  going to execute the code inside of the default statement.
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
